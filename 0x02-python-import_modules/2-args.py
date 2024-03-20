f371b8465c66a8698c8c2a75ccdf1dd7870c5e79
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-03-20 10:40:59 -0700

    Task2:Fix: change first prompt

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import sys
 if (__name__ == "__main__"):
-    if (len(sys.argv) == 1):
+    argc = len(sys.argv)
+    if (argc == 1):
         print("0 arguments.")
     else:
-        print("{} arguemnts:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
+        if (argc == 2):
+            print("{} argument:".format(argc - 1))
+        else:
+            print("{} arguemnts:".format(argc - 1))
+        for i in range(1, argc):
             print("{}: {}".format(i, sys.argv[i]))
