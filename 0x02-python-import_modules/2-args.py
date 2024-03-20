238864dfd23c4119460743a7bde4c78a4348f32d
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-03-20 10:45:42 -0700

    Task2:Fix: spelling of arguments in multi arguments case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if (__name__ == "__main__"):
         if (argc == 2):
             print("{} argument:".format(argc - 1))
         else:
-            print("{} arguemnts:".format(argc - 1))
+            print("{} arguments:".format(argc - 1))
         for i in range(1, argc):
             print("{}: {}".format(i, sys.argv[i]))
