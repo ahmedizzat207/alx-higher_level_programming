9382cb2d394d3adb28dcf018918dad7f44cb65e6
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-03-25 00:47:47 -0700

    Task6:Add: 6-print_matrix_integer.py file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = []
-    for i in range(len(my_string)):
-        if (my_string[i] != 'c') and (my_string[i] != 'C'):
-            new_string.append(my_string[i])
-    return ''.join(new_string)
+    new_string = ""
+    for char in my_string:
+        if (char != 'c') and (char != 'C'):
+            new_string += char
+    return new_string
