6231f23b9e41e71ab114c6413a58ab5073d3a203
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-03-25 00:49:55 -0700

    Task6:Fix: use integer specifier in str.format()

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             end = " " if (j != i[len(i) - 1]) else ""
-            print("{}".format(j), end=end)
+            print("{:d}".format(j), end=end)
         print()
