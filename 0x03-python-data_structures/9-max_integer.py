6b7dfd85627c45d92407d31899708b6bc1e6e2d1
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-03-26 01:49:28 -0700

    Task9:Fix: solve empty lists

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,10 @@ def max_integer(my_list=[]):
     """my_list.sort()
     return my_list[len(my_list) - 1]
     """
-    max_value = my_list[0]
+    if (my_list):
+        max_value = my_list[0]
+    else:
+        max_value = None
     for i in range(0, len(my_list)):
         if max_value < my_list[i]:
             max_value = my_list[i]
