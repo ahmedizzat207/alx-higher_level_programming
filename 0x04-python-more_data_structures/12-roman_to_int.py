5323f2aacee41abc0269c5a0ab0e6b5bb6cd7acb
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-03-28 01:58:35 -0700

    Task12:Add: 12-roman_to_int.py file

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is str:
+        result = 0
+        numbers = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10,
+                   'V': 5, 'I': 1}
+        charnum = 0
+        while (charnum < len(roman_string)):
+            char = roman_string[charnum]
+            if char in numbers:
+                next_value = 0
+                if (charnum + 1) < len(roman_string):
+                    next_value = numbers[roman_string[charnum + 1]]
+                if numbers[char] < next_value:
+                    result += next_value - numbers[char]
+                else:
+                    result += numbers[char]
+            else:
+                break
+            charnum += 1
+        return result
+    else:
+        return None
