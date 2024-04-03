5a05c4e9164baa8b0ac6e3efd4f18a8087411a1a
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-04-02 19:51:29 -0700

    Task3:Fix: add finally and use string formating

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,4 +4,5 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         result = None
-    print("inside result: ", result)
+    finally:
+        print("inside result: {}".format(result))
