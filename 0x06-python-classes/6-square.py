a8be1ef2e4284ee860f6aa3f883827195fcf91b4
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-04-21 04:10:48 -0700

    Task6:Fix: check position parameters for positive and total of 2 parameters

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,8 +33,11 @@ class Square:
     @position.setter
     def position(self, value):
         if (isinstance(value, tuple) and
+           len(value) == 2 and
            isinstance(value[0], int) and
-           isinstance(value[1], int)):
+           isinstance(value[1], int) and
+           value[0] < 0 and
+           value[1] < 0):
             self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
