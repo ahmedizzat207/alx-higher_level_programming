a7cdd68eca44aa0d2867c139880c844fc14344eb
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-04-21 00:35:13 -0700

    Task1:Add: 1-square.py file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""This module specified for Task 1:Square with size"""
+
+
+class Square:
+    """Square Object Class
+
+    The class dedicated for squares related computations
+    """
+    def __init__(self, size):
+        """instantiation of with size argument for the class"""
+        self.__size = size
