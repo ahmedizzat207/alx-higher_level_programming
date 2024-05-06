be5aaf0e26539854dc7d56cb1f11055cbb6931c6
Ahmed Izzat <ahmedizzat207@gmail.com>
2024-05-06 09:37:05 -0700

    Task0:Fix: add documentation for the 0-add_integer.py module and importing path

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This module for Task0: Integers addition"""
+
+
 def add_integer(a, b=98):
+    """This function adds two integer numbers only"""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
